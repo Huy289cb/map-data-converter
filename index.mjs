d607763ec0857679bc9ae05cdbb90872b2dd3fc2
huy289cb <luonghuyn2cb@gmail.com>
2023-01-06 09:16:58 +0700

    parsed json districts & provinces

--- index.mjs
+++ index.mjs
@@ -1,9 +1,14 @@
 import fs from "fs";
-import provinces from "./districts.js";
 import axios from "axios";
-const path = "./mapDataJSON/district/";
 
-const token = "eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJlUktVWG10TFhKMHBBNkxBS29aWko1ZlU0VDhCdmxKdERCb3pXanFFdnhjIn0.eyJleHAiOjE2NzI5NjEwNzgsImlhdCI6MTY3MjkyNTA3OCwiYXV0aF90aW1lIjoxNjcyOTI1MDc4LCJqdGkiOiIxYmY3ZWI2Mi1mYTNhLTQ4NmEtYWRjMC0wMWQ3ODRlYjAwM2MiLCJpc3MiOiJodHRwczovL3Nzby5nbG9iaXRzLm5ldC9hdXRoL3JlYWxtcy9nbG9iaXRzIiwic3ViIjoiMDQzNTA1MzEtNWJmZC00YjQ4LWJjMmEtODliNThlYzEwM2M5IiwidHlwIjoiQmVhcmVyIiwiYXpwIjoic3NvQ2xpZW50LTEiLCJub25jZSI6IjgyZjEyYjdhLTI1ZDYtNGI4YS04MzM4LTE2ZmYyNjFiYTRjOSIsInNlc3Npb25fc3RhdGUiOiI1MzEzNDQ5NS1hMzhkLTQwMDktOGRhNi0xMDJiNDFiYzUxYmMiLCJhY3IiOiIxIiwic2NvcGUiOiJvcGVuaWQgcHJvZmlsZSIsInByZWZlcnJlZF91c2VybmFtZSI6ImFkbWluIn0.r2OnuZrEae4wqRQ4PihvQxvOleDjqIxpyEhbq3Sz6miebOaZWD7jX5_w4c97qqtxj84UaywY-Ttl1Buf4ZfEvM9iqJ9QIbYQtxmceg-S13-egyiJI9zvsW8tSDWLhIXnF3honB2DJ7zgxaTYuQztO8lJNLBQNCuyu00pj15KIYTMEjfUB3AylP1yc7Xu06_mWwxqP8S06yY9uqSWj7d_ZA4Wy0l6TMLtJZzC0z8QZb5-xtR22u1z1R8fyDqrmA_MxwDMmP0fWEker6cu11MZPe_9DUdvN8qrqWqiSo-t585Gv5dT0NVqVSNHvC0BageAIbbGLRx_AhO9j36_5sGkyA";
+// import provinces from "./districts.js";
+import { vi_all as listProvinces } from "./mapData/gadm36_VNM_1.js";
+// import { VNM_all as listDistricts } from "./mapData/gadm36_VNM_2.js";
+
+
+const path = "./mapDataJSON/";
+
+const token = "eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJlUktVWG10TFhKMHBBNkxBS29aWko1ZlU0VDhCdmxKdERCb3pXanFFdnhjIn0.eyJleHAiOjE2NzMwMDY4NTAsImlhdCI6MTY3Mjk3MDkyNSwiYXV0aF90aW1lIjoxNjcyOTcwOTI0LCJqdGkiOiIzZGM3M2JiNS0xNmJiLTQwMmMtYTYwNy0xYTBiNmE4NjEzODkiLCJpc3MiOiJodHRwczovL3Nzby5nbG9iaXRzLm5ldC9hdXRoL3JlYWxtcy9nbG9iaXRzIiwic3ViIjoiMDQzNTA1MzEtNWJmZC00YjQ4LWJjMmEtODliNThlYzEwM2M5IiwidHlwIjoiQmVhcmVyIiwiYXpwIjoic3NvQ2xpZW50LTEiLCJub25jZSI6ImE0MTBiMjQ1LTQ0N2MtNGZiMy1iNTBhLTNlOGNmZDdlODllYSIsInNlc3Npb25fc3RhdGUiOiIzMmU1Y2M5MC0yM2IwLTQ1OWQtYjUyOS04MGNlNGNhOTU4M2YiLCJhY3IiOiIxIiwic2NvcGUiOiJvcGVuaWQgcHJvZmlsZSIsInByZWZlcnJlZF91c2VybmFtZSI6ImFkbWluIn0.ECAdC13hexXYOJhiTDpX_Vf4kqnzBRcMihErsBOs9NJxmHsJ0XW5-kwr3v8upUkFTgcWoht8UtDRdt6S-1btixVat672ADcg2DA-_YXgpRtlCXSf9-QYZTupkErsnp__8FCqHyDVNnLBrj2UWv7GL560yvToGGGDAJLCBSVNvgS8NFih-WKsIL-6Lq6Jiey_KCuM-GGgLQmcrAHUsfgAoPv6QzRcCsDn6Xn6zB7h6wMajWt9U_uQnm5AcNF7N2rdHPsuy_hq7XajikNh30431ZCZZQtJ6ZH_6fRHJE9UnekDkNS8J3dmsERTiHIhmopUnLR3g7qilAmQRcDJQdNgYg";
 axios.defaults.headers.common["Authorization"] = "Bearer " + token;
 
 const API_PATH = "http://localhost:8071/api/administrative-unit";
@@ -60,7 +65,7 @@ const pagingAdministratives = (searchObject) => {
 //                   return ward
 //                 })
 //                 const data = JSON.stringify({...province, features: covertedWards});
-//                 fs.writeFile(path + provinceName + ".json", data, (err) => {
+//                 fs.writeFile(path + "district/" + provinceName + ".json", data, (err) => {
 //                   if (err) console.log(err + provinceName);
 //                   console.log("Successfully Written to File." + provinceName);
 //                 });
@@ -74,4 +79,88 @@ const pagingAdministratives = (searchObject) => {
 // }
 // parseWards();
 
+// const parseDistricts = async _ => {
+//   pagingAdministratives({
+//     level: 4,
+//     pageIndex: 1,
+//     pageSize: 9999,
+//   }).then((res) => {
+//     if (res && res.data && res.data.content && res.data.content.length > 0) {
+//       const districts = listDistricts.features
+//       const listDistrictsVitimes = res.data.content;
+//       const covertedDistricts = districts.map((dt) => {
+//         const findVitimesDt = listDistrictsVitimes.find(f => {
+//           if (dt.properties && dt.properties.NAME_1 && dt.properties.NAME_2 && f.name && f.parentName) {
+//             if (f.name.includes(dt.properties.NAME_2) && f.parentName.includes(dt.properties.NAME_1)) {
+//               return true
+//             }
+//           }
+//           return false;
+//         })
+//         if (findVitimesDt) {
+//           return {
+//             ...dt,
+//             properties: {
+//              ...dt.properties,
+//              code1: findVitimesDt.parentCode,
+//              code2: findVitimesDt.code
+//             },
+//           }
+//         }
+//         return dt
+//       })
+//       const data = JSON.stringify({...listDistricts, features: covertedDistricts});
+//       fs.writeFile(path + "gadm36_VNM_2.json", data, (err) => {
+//         if (err) console.log(err + "gadm36_VNM_2");
+//         console.log("Successfully Written to File." + "gadm36_VNM_2");
+//       });
+//     }
+//   }).catch(err => {
+//     console.log(err);
+//   })
 
+// }
+// parseDistricts();
+
+const parseProvinces = async _ => {
+  pagingAdministratives({
+    level: 3,
+    pageIndex: 1,
+    pageSize: 9999,
+  }).then((res) => {
+    if (res && res.data && res.data.content && res.data.content.length > 0) {
+      const provinces = listProvinces.features
+      const listProvincesVitimes = res.data.content;
+      const covertedProvinces = provinces.map((p) => {
+        const findVitimesDt = listProvincesVitimes.find(f => {
+          if (p.properties && p.properties.NAME_1 && f.name) {
+            if (f.name.includes(p.properties.NAME_1)) {
+              return true
+            }
+          }
+          return false;
+        })
+        if (findVitimesDt) {
+          return {
+            ...p,
+            properties: {
+             ...p.properties,
+             code1: findVitimesDt.parentCode,
+             code2: findVitimesDt.code
+            },
+          }
+        }
+        return p
+      })
+      const data = JSON.stringify({...listProvinces, features: covertedProvinces});
+      fs.writeFile(path + "gadm36_VNM_1.json", data, (err) => {
+        if (err) console.log(err + "gadm36_VNM_1");
+        console.log("Successfully Written to File." + "gadm36_VNM_1");
+      });
+    }
+  }).catch(err => {
+    console.log(err);
+  })
+
+}
+parseProvinces();
